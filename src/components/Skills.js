ed7161f6b85fafc5a57b6cf39b8259891c7f0f10
shrutika142000 <shrutijuvale14@gmail.com>
2024-10-17 19:58:15 +0530

    Add files via upload

---
+++ src/components/Skills.js
@@ -0,0 +1,23 @@
+import React from "react";
+const Skills = () => (
+  <section id="skills" className="bg-dark text-light py-5">
+    <div className="container text-center">
+      <h2>Skills</h2>
+      <div className="row mt-4">
+        <div className="col-md-3">
+          <h4>HTML</h4>
+        </div>
+        <div className="col-md-3">
+          <h4>CSS</h4>
+        </div>
+        <div className="col-md-3">
+          <h4>JavaScript</h4>
+        </div>
+        <div className="col-md-3">
+          <h4>React.js</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+export default Skills;
